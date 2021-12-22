7d6e7dbff51574b52714dbdcb26fec05be8bc3c5
adarsh <adarsh986030@gmail.com>
2021-12-22 22:45:49 +0545

    [updated] updated the assignments list

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const assignmentTableBody = document.querySelector('#assignment-table-body')
 const assignmentData = [
   {
     title: 'Semantic HTML',
-    source: 'http://github.com',
+    source:
+      'https://github.com/adarshmaharjan/Webpoint-Assignments/tree/gh-pages/assignments/html_css/semantic_html',
     liveDemo: './assignments/html_css/semantic_html/semantic.html',
     content: {
       html: true,
@@ -13,23 +14,161 @@ const assignmentData = [
     },
   },
   {
-    title: 'Assignment 2',
-    source: 'http://github.com',
-    liveDemo: 'http://google.com',
+    title: 'Signup Form',
+    source:
+      'https://github.com/adarshmaharjan/Webpoint-Assignments/tree/gh-pages/assignments/html_css/signup_form',
+    liveDemo: './assignments/html_css/signup_form',
     content: {
-      html: false,
+      html: true,
+      css: false,
+      js: false,
+    },
+  },
+  {
+    title: 'Selectors in CSS',
+    source:
+      'https://github.com/adarshmaharjan/Webpoint-Assignments/tree/gh-pages/assignments/html_css/selectors',
+    liveDemo: './assignments/html_css/selectors/index.html',
+    content: {
+      html: true,
       css: true,
       js: false,
     },
   },
   {
-    title: 'Assignment 3',
-    source: 'http://github.com',
-    liveDemo: 'http://google.com',
+    title: 'Slicing figma design',
+    source:
+      'https://github.com/adarshmaharjan/Webpoint-Assignments/tree/gh-pages/assignments/html_css/matter_segment_slicing',
+    liveDemo: './assignments/html_css/matter_segment_slicing/index.html',
     content: {
       html: true,
-      css: false,
-      js: true,
+      css: true,
+      js: false,
+    },
+  },
+  {
+    title: 'Internal CSS',
+    source:
+      'https://github.com/adarshmaharjan/Webpoint-Assignments/tree/gh-pages/assignments/html_css/internal_css',
+    liveDemo: './assignments/html_css/internal_css/index.html',
+    content: {
+      html: true,
+      css: true,
+      js: false,
+    },
+  },
+  {
+    title: 'Inline CSS',
+    source:
+      'https://github.com/adarshmaharjan/Webpoint-Assignments/tree/gh-pages/assignments/html_css/inline_css',
+    liveDemo: './assignments/html_css/inline_css/index.html',
+    content: {
+      html: true,
+      css: true,
+      js: false,
+    },
+  },
+  {
+    title: 'Google Fonts',
+    source:
+      'https://github.com/adarshmaharjan/Webpoint-Assignments/tree/gh-pages/assignments/html_css/google_fonts',
+    liveDemo: './assignments/html_css/google_fonts/index.html',
+    content: {
+      html: true,
+      css: true,
+      js: false,
+    },
+  },
+  {
+    title: 'External CSS',
+    source:
+      'https://github.com/adarshmaharjan/Webpoint-Assignments/tree/gh-pages/assignments/html_css/external_css',
+    liveDemo: './assignments/html_css/external_css/index.html',
+    content: {
+      html: true,
+      css: true,
+      js: false,
+    },
+  },
+  {
+    title: 'CSS variable and math function',
+    source:
+      'https://github.com/adarshmaharjan/Webpoint-Assignments/tree/gh-pages/assignments/html_css/css-variables_and_math-function',
+    liveDemo:
+      './assignments/html_css/css-variables_and_math-function/index.html',
+    content: {
+      html: true,
+      css: true,
+      js: false,
+    },
+  },
+  {
+    title: 'CSS specificity, Pseudo class and Pseudo element',
+    source:
+      'https://github.com/adarshmaharjan/Webpoint-Assignments/tree/gh-pages/assignments/html_css/css-specificity_pseudo-class_pseudo-elements',
+    liveDemo:
+      './assignments/html_css/css-specificity_pseudo-class_pseudo-elements/index.html',
+    content: {
+      html: true,
+      css: true,
+      js: false,
+    },
+  },
+  {
+    title: 'Elements that dont need Closing tags',
+    source:
+      'https://github.com/adarshmaharjan/Webpoint-Assignments/tree/gh-pages/assignments/html_css/closing_tags',
+    liveDemo: './assignments/html_css/closing_tags/index.html',
+    content: {
+      html: true,
+      css: true,
+      js: false,
+    },
+  },
+  {
+    title: 'Button Element',
+    source:
+      'https://github.com/adarshmaharjan/Webpoint-Assignments/tree/gh-pages/assignments/html_css/button',
+    liveDemo: './assignments/html_css/button/index.html',
+    content: {
+      html: true,
+      css: true,
+      js: false,
+    },
+  },
+  {
+    title: 'Typography, List, Table, Circle',
+    source:
+      'https://github.com/adarshmaharjan/Webpoint-Assignments/blob/gh-pages/assignments/html_css/basic_html_css/index.html',
+    liveDemo: './assignments/html_css/basic_html_css/index.html',
+    content: {
+      html: true,
+      css: true,
+      js: false,
+    },
+  },
+  {
+    title: 'OOP and SOLID principles',
+    source:
+      'https://github.com/adarshmaharjan/Webpoint-Assignments/tree/gh-pages/assignments/html_css/css-specificity_pseudo-class_pseudo-elements',
+    liveDemo:
+      './assignments/html_css/css-specificity_pseudo-class_pseudo-elements/index.html',
+    content: {
+      html: true,
+      css: true,
+      js: false,
+    },
+  },
+  {
+    title: 'CSS specificity, Pseudo class and Pseudo element',
+    source:
+      'https://github.com/adarshmaharjan/Webpoint-Assignments/tree/gh-pages/assignments/html_css/css-specificity_pseudo-class_pseudo-elements',
+    liveDemo:
+      './assignments/html_css/css-specificity_pseudo-class_pseudo-elements/index.html',
+    content: {
+      html: true,
+      css: true,
+      js: false,
     },
   },
 ]
@@ -54,16 +193,25 @@ const getAssignmentContent = () => {
     return `
     <tr>
         <td>${title}</td>
-        <td><a href=${source} target="_blank" rel="noopener noreferrer">Hosted of github</a></td>
-        <td><a href=${liveDemo} target="_blank" rel="noopener noreferrer">Live Demo</a></td>
+        <td>
+          <a href=${source} target="_blank" rel="noopener noreferrer">
+          Hosted of github
+          </a>
+        </td>
+        <td>
+          <a href=${liveDemo} target="_blank" rel="noopener noreferrer">
+          Live Demo
+          </a>
+        </td>
         <td>${topic}</td>
-    </tr>`
+    </tr>
+    `
   })
-  return assignmentContent.join('')
+  return assignmentContent
 }
 // console.log(getAssignmentContent())
 const renderAssignment = () => {
-  assignmentTableBody.innerHTML = getAssignmentContent()
+  assignmentTableBody.innerHTML = getAssignmentContent().join('')
 }
 // execution
 
